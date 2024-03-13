e66caba3cde2007cf7db2bdf427b4b0f8fd3b654
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-03-13 14:19:18 +0400

    configured .env for handling enviroment variables both for locally and for Docker

--- fitness_planner/fitness_planner/settings.py
+++ fitness_planner/fitness_planner/settings.py
@@ -1,38 +1,32 @@
 # settings.py
 
 import os
-from datetime import timedelta
+import environ
+
+env = environ.Env()
+# Reading .env file
+environ.Env.read_env()
+
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'sixdf_*7jc)$e2)pl@6fpg#ak((oc#5#nplig6r*(tshwk%1jz'
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_DATABASE', 'fitness_planner'),
-        'USER': os.environ.get('POSTGRES_USERNAME','postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD','messi10'),
+        'USER': os.environ.get('POSTGRES_USERNAME', 'postgres'),  
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'messi10'),  
         'HOST': os.environ.get('POSTGRES_REMOTE_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_REMOTE_PORT','5432'),
+        'PORT': os.environ.get('POSTGRES_REMOTE_PORT', '5432'),
         'OPTIONS': {'sslmode': 'disable'}
+
     }
 }
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'Authorization',
-            'in': 'header'
-        }
-    }
-
-}
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -42,19 +36,46 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_yasg',
+    'drf_spectacular_sidecar',
+    'drf_spectacular',
     'rest_framework_simplejwt',
-    'user',
+    'user',  
     'fitness_planner'
 ]
-
-
+from datetime import timedelta
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # Other authentication classes if needed
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+# Whitenoise to serve static files.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+SPECTACULAR_SETTINGS = {
+
+    "TITLE": "Fitness Tracker",
+    "VERSION": 'v1',
+    "DESCRIPTION": "APIs to enable fitness tracking and enable workour plan creation",
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+    'SECURITY': [
+        {
+            'Bearer': []
+        }
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+# Path were Django will look for static directory and CSS-Styling.
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')  # Path for django to look static at.
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=100),
@@ -64,6 +85,7 @@ SIMPLE_JWT = {
 ROOT_URLCONF = 'fitness_planner.urls'
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware'
 ]
